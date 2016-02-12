ee59c7a49a3c85f2a50c8316e08ce9174af4924c
Samantha Hill <samchill@samanthas-air.cate.org>
2016-02-12 09:37:58 -0800

    deleted test functions

--- black_jack.py
+++ black_jack.py
@@ -138,15 +138,7 @@ if __name__ == '__main__':
     hand.sort()
     print hand
 
-def dick():
-    return '8===D'
 
-
-def test_function(a,b,c):
-    """This is a test function for git hub"""
-
-    pass
-    
 '''
 git add -A #any file added into folder added to personal repository
 git commit -m 'I changed...' #tells other coder what you edited
